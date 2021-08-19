83c282391ecfcac36676f5b74c7f29698e9c9271
LuanaPorciuncula <lpb@cin.ufpe.br>
2021-08-19 18:12:21 -0300

    working find restaurant

--- rasa_chatbot/actions/actions.py
+++ rasa_chatbot/actions/actions.py
@@ -28,7 +28,7 @@ class RestaurantAPI:
 
         print(food, area, pricerange)
         options = [x for x in filter(lambda x: x["food"] == food and x["area"] in area and x["pricerange"] == pricerange, restaurants)]
-        
+
         random.shuffle(options)
         options[:min(3, len(options))]
 
@@ -42,7 +42,26 @@ class ActionFindRestaurants(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="looking for restaurants")
+        food = tracker.get_slot("restaurant_food")
+        area = tracker.get_slot("restaurant_area")
+        pricerange = tracker.get_slot("restaurant_pricerange")
+        utter_looking_4_res = f'Searching for {food} restaurants in the {area} with a {pricerange} price...' 
+        
+        dispatcher.utter_message(text=utter_looking_4_res)
         restaurant_api = RestaurantAPI()
         res = restaurant_api.filter_res(tracker)
         return [SlotSet("restaurant_list", res)]
+
+class ActionShowOptions(Action):
+    def name(self):
+        return "action_show_options"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        res = tracker.get_slot("restaurant_list")
+
+        buttons = [{"title": x["name"], "payload": '/affirm{"name":"' + x["name"] + '"}'} for x in res] + [{"title": "No option", "payload": "/goodbye"}]
+        dispatcher.utter_message(text="here's what I found:", buttons=buttons)
+
+        return []
